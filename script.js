47967dc67f8699f095e87fc7fb36346b03b75ac2
Kuntal Ojha <kuntalojha2021@gmail.com>
2022-03-11 02:06:43 +0530

    change

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 fetch('https://icanhazdadjoke.com/slack')
-      .then(data => data.json())
-      .then(jokeData =>{
-        const jokeText = jokeData.attachments[2].text;
+    .then(data => data.json())
+    .then(jokeData => {
+        const jokeText = jokeData.attachments[0].text;
         const jokeElement = document.getElementById('jokeElement');
 
         jokeElement.innerHTML = jokeText;
-      })
+    })
